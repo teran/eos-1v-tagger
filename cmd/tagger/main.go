02477a6172b04289d517f6cd8ea897964deb1799
Igor Shishkin <me@teran.ru>
2019-10-09 13:58:55 +0300

    exiftool commands printing PoC
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- cmd/tagger/main.go
+++ cmd/tagger/main.go
@@ -1,10 +1,11 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
 	"log"
 	"os"
+	"strings"
+	"time"
 
 	tagger "github.com/teran/eos-1v-tagger"
 )
@@ -20,10 +21,40 @@ func main() {
 		log.Fatalf("error parsing CSV: %s", err)
 	}
 
-	j, err := json.Marshal(film)
-	if err != nil {
-		log.Fatalf("error marshaling JSON: %s", err)
+	for _, f := range film.Frames {
+		fmt.Printf(
+			strings.Join([]string{
+				`exiftool`,
+				`-overwrite_original`,
+				`-FocalLength="%dmm"`,
+				`-ExposureTime="%s"`,
+				`-ShutterSpeedValue="%s"`,
+				`-FNumber="%v"`,
+				`-ApertureValue="%v"`,
+				`-ISO="%d"`,
+				`-ISOSpeed="%d"`,
+				`-ExposureCompensation="%v"`,
+				`-MeteringMode="%v"`,
+				`-ShootingMode="%v"`,
+				`-FocusMode="%v"`,
+				`-DateTimeOriginal="%s"`,
+				`-ModifyDate="%s"`,
+				`FILM_%05d.dng`,
+			}, " ")+"\n",
+			f.FocalLength,
+			f.Tv,
+			f.Tv,
+			f.Av,
+			f.Av,
+			f.ISO,
+			f.ISO,
+			f.ExposureCompensation,
+			f.MeteringMode,
+			f.ShoothingMode,
+			f.AFMode,
+			f.Timestamp.Format(time.RFC3339),
+			f.Timestamp.Format(time.RFC3339),
+			f.Number,
+		)
 	}
-
-	fmt.Println(string(j))
 }
