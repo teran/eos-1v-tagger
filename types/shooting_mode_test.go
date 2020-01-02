fd36f39505c0ef44f71521f21dcd4162dbf259a4
Igor Shishkin <me@teran.ru>
2020-01-02 05:53:18 +0300

    Cover stringer with test for shooting mode
    Signed-off-by: Igor Shishkin <me@teran.ru>

--- types/shooting_mode_test.go
+++ types/shooting_mode_test.go
@@ -14,6 +14,7 @@ func TestShootingMode(t *testing.T) {
 		name      string
 		input     string
 		expOutput *ShootingMode
+		expString string
 		expError  error
 	}
 
@@ -22,36 +23,43 @@ func TestShootingMode(t *testing.T) {
 			name:      "Program AE",
 			input:     "Program AE",
 			expOutput: ptrShootingMode(ShootingModeProgramAE),
+			expString: "Program AE",
 		},
 		{
 			name:      "Shutter-speed-priority AE",
 			input:     "Shutter-speed-priority AE",
 			expOutput: ptrShootingMode(ShootingModeShutterSpeedPriorityAE),
+			expString: "Shutter-speed-priority AE",
 		},
 		{
 			name:      "Aperture-priority AE",
 			input:     "Aperture-priority AE",
 			expOutput: ptrShootingMode(ShootingModeAperturePriorityAE),
+			expString: "Aperture-priority AE",
 		},
 		{
 			name:      "Depth-of-field AE",
 			input:     "Depth-of-field AE",
 			expOutput: ptrShootingMode(ShootingModeDepthOfFieldAE),
+			expString: "Depth-of-field AE",
 		},
 		{
 			name:      "Manual exposure",
 			input:     "Manual exposure",
 			expOutput: ptrShootingMode(ShootingModeManualExposure),
+			expString: "Manual exposure",
 		},
 		{
 			name:      "Bulb",
 			input:     "Bulb",
 			expOutput: ptrShootingMode(ShootingModeBulb),
+			expString: "Bulb",
 		},
 		{
 			name:      "Program AE with spaces",
 			input:     "      Program AE    ",
 			expOutput: ptrShootingMode(ShootingModeProgramAE),
+			expString: "Program AE",
 		},
 		{
 			name:     "some random text",
@@ -69,6 +77,7 @@ func TestShootingMode(t *testing.T) {
 		sm, err := ShootingModeFromString(tc.input)
 		if tc.expError == nil {
 			r.Equalf(tc.expOutput, sm, tc.name)
+			r.Equalf(tc.expString, sm.String(), tc.name)
 			r.NoErrorf(err, tc.name)
 		} else {
 			r.Errorf(err, tc.name)
